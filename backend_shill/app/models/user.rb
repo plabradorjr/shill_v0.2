977e79ce9b976a34e8413381c071987dc8d7eae5
Pancho Labrador <plabradorjr@gmail.com>
2020-12-29 21:31:42 -0600

    added comment schema and model

--- backend_shill/app/models/user.rb
+++ backend_shill/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates_uniqueness_of :uid
+
+    has_many :comments
 end
--- backend_shill/db/schema.rb
+++ backend_shill/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_223222) do
+ActiveRecord::Schema.define(version: 2020_12_30_032652) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
