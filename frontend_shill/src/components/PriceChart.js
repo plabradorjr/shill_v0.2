dd6331ede2f4b6d9fae7c109654969f28b985403
Pancho Labrador <plabradorjr@gmail.com>
2021-01-10 07:38:28 -0600

    high charts now dark and red candle

--- frontend_shill/src/components/PriceChart.js
+++ frontend_shill/src/components/PriceChart.js
@@ -8,24 +8,46 @@ const PriceChart = ({data}) => {
 
 
     const options = {
-
+    colors: ['#eb2f06',
+            '#0eda63',
+            '#14bb46',
+            '#DDDF00', 
+            '#24c90f',
+            '#64E572',   
+            '#1ed461',
+            '#FFF263',
+            '#0eda63'],
+    chart: {
+        backgroundColor: {
+            linearGradient: [0, 0, 500, 500],
+            stops: [
+                [0, 'rgb(164, 164, 175)'],
+                [1, 'rgb(164, 164, 175)']
+            ]
+        },
+    },
     rangeSelector: {
         selected: 1
     },
-
+    legend: {
+        itemStyle: {
+            font: '9pt Trebuchet MS, Verdana, sans-serif',
+            color: 'white'
+        },
+        itemHoverStyle:{
+            color: 'white'
+        }   
+    },
     title: {
         text: ''
     },
-    
     credits: {
         enabled: false,
     },
-
     series: [{
         type: 'candlestick',
         name: '🚀',
         data: data,
-        
     }]
     }
 
