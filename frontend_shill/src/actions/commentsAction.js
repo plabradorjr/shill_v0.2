836a119ee055d23e58fa4736ecfbb6801f54b415
Pancho Labrador <plabradorjr@gmail.com>
2021-01-11 08:31:26 -0600

    using fetch() instead of axios

--- frontend_shill/src/actions/commentsAction.js
+++ frontend_shill/src/actions/commentsAction.js
@@ -1,13 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
+// note to self, if not using axios, you need to delete ".data" on the payload dispatch
 
 export const fetchComments = () => async (dispatch) => {
 
-    const allComments = await axios.get("http://localhost:3030/comments")
+    // const allComments = await axios.get("http://localhost:3030/comments")
+
+    const allComments = await fetch("http://localhost:3030/comments").then(res => res.json())
 
     dispatch({
         type: "FETCH_COMMENTS",
         payload: {
-            comments: allComments.data
+            comments: allComments
         }
     })
 }
